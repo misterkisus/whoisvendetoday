9104f2c0161a8b23175c471ddb146ad5db0741ee
misterkisus <88712927+misterkisus@users.noreply.github.com>
2023-03-21 12:32:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -55,15 +55,20 @@ function savePhrase(phrase) {
     return localStorage.getItem('phrase');
   }
 
+  function changeBackgroundImage() {
+    document.body.style.backgroundImage = "url('https://i.ibb.co/qr5TQT9/photo-2022-02-17-20-35-03-2.jpg')";
+  }
+
   generateButton.addEventListener('click', function () {
-  const phrases = ['казел', 'казлина', 'говнюк', "пидарас", "тварь", "дерьмоед", "уебан", "тупень", "эммм", "влагалище", "уебанище", "пидорасина", "не сдох к сожалению", "сдох"];
-  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-  const heading = document.querySelector('h1');
-  heading.innerHTML = `Венде сегодня ${randomPhrase}`;
-  animateText(heading);
-  disableButton();
-  savePhrase(heading.innerHTML);
-});
+    const phrases = ['казел', 'казлина', 'говнюк'];
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    const heading = document.querySelector('h1');
+    heading.innerHTML = `Ваня сегодня ${randomPhrase}`;
+    animateText(heading);
+    disableButton();
+    savePhrase(heading.innerHTML);
+    changeBackgroundImage();
+  });
 
 document.addEventListener('DOMContentLoaded', function () {
   const nextClickTime = localStorage.getItem('nextClickTime');
